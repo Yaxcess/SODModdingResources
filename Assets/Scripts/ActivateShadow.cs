ad09efc851e49deccc0a4612a78990e4ce6bb945
Yaxcess <scrap.the.wolficy@gmail.com>
2023-10-29 17:04:08 +0200

    Added missing PfAlienShadow

---
+++ Assets/Scripts/ActivateShadow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// Token: 0x02000598 RID: 1432
+public class ActivateShadow : MonoBehaviour
+{
+	// Token: 0x170003A1 RID: 929
+	// (set) Token: 0x060026D1 RID: 9937 RVA: 0x0001E7FD File Offset: 0x0001C9FD
+	public bool pForceProjectionShadow;
+
+	
+}
